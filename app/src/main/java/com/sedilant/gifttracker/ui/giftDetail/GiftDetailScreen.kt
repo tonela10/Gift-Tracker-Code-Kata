ffce8893635d064e54cd19283c6f887c8b48c6e1
Antonio <torosediles10@gmail.com>
2025-12-01 17:47:43 +0100

    Refactor GiftDetailScreen and extract string resources

--- app/src/main/java/com/sedilant/gifttracker/ui/giftDetail/GiftDetailScreen.kt
+++ app/src/main/java/com/sedilant/gifttracker/ui/giftDetail/GiftDetailScreen.kt
@@ -24,11 +24,13 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.sedilant.gifttracker.R
 import com.sedilant.gifttracker.ui.theme.BackgroundLight
 
 @Composable
@@ -66,63 +68,18 @@ public fun GiftDetailScreenStateless(
 ) {
     Scaffold(
         topBar = {
-            TopAppBar(
-                modifier = Modifier.background(BackgroundLight),
-                title = { Text("Detalle del Regalo", fontWeight = FontWeight.Bold) },
-                navigationIcon = {
-                    // TODO change the animation
-                    Crossfade(
-                        targetState = uiState.isEditMode,
-                        label = "nav-icon-animation"
-                    ) { isEditMode ->
-                        if (isEditMode) {
-                            IconButton(onClick = { onToggleEditMode() }) {
-                                Icon(
-                                    imageVector = Icons.Default.Close,
-                                    contentDescription = "Salir de edición"
-                                )
-                            }
-                        } else {
-                            IconButton(onClick = onNavigateBack) {
-                                Icon(
-                                    imageVector = Icons.Default.ArrowBack,
-                                    contentDescription = "Volver"
-                                )
-                            }
-                        }
-                    }
-                }
+            DetailsTopBar(
+                isEditMode = uiState.isEditMode,
+                onToggleEditMode = onToggleEditMode,
+                onNavigateBack = onNavigateBack
             )
         },
         bottomBar = {
-            // TODO change the animation for another cool one
-            Crossfade(
-                targetState = uiState.isEditMode,
-                label = "bottom-bar-animation"
-            ) { isEditMode ->
-                val buttonColor = if (isEditMode) Color(0xFF4CAF50) else Color(0xFFC62828)
-                val buttonText = if (isEditMode) "Guardar" else "Editar"
-                val onClickAction = if (isEditMode) {
-                    { onSaveGiftDetails() }
-                } else {
-                    { onToggleEditMode() }
-                }
-
-                Button(
-                    onClick = onClickAction,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp),
-                    shape = RoundedCornerShape(50),
-                    colors = ButtonDefaults.buttonColors(containerColor = buttonColor)
-                ) {
-                    Text(
-                        text = buttonText,
-                        modifier = Modifier.padding(8.dp),
-                        fontSize = 16.sp
-                    )
-                }
-            }
+            DetailsBottomBar(
+                isEditMode = uiState.isEditMode,
+                onToggleEditMode = onToggleEditMode,
+                onSaveGiftDetails = onSaveGiftDetails
+            )
         }
     ) { paddingValues ->
         Column(
@@ -134,24 +91,102 @@ public fun GiftDetailScreenStateless(
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             DetailItemCard(
-                label = "Persona",
+                label = stringResource(id = R.string.person_label),
                 value = uiState.person,
                 isEditMode = uiState.isEditMode,
                 onValueChange = { onPersonChange(it) }
             )
             DetailItemCard(
-                label = "Regalo",
+                label = stringResource(id = R.string.gift_label),
                 value = uiState.gift,
                 isEditMode = uiState.isEditMode,
                 onValueChange = { onGiftChange(it) }
             )
             DetailItemCard(
-                label = "Precio en €",
+                label = stringResource(id = R.string.price_label),
                 value = uiState.price,
                 isEditMode = uiState.isEditMode,
                 onValueChange = { onPriceChange(it) }
             )
             StatusItemCard(isPurchased = uiState.isPurchased)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun DetailsTopBar(
+    isEditMode: Boolean,
+    onToggleEditMode: () -> Unit,
+    onNavigateBack: () -> Unit
+) {
+    TopAppBar(
+        modifier = Modifier.background(BackgroundLight),
+        title = {
+            Text(
+                stringResource(id = R.string.gift_detail_title),
+                fontWeight = FontWeight.Bold
+            )
+        },
+        navigationIcon = {
+            // TODO change the animation
+            Crossfade(
+                targetState = isEditMode,
+                label = "nav-icon-animation"
+            ) { isEditMode ->
+                if (isEditMode) {
+                    IconButton(onClick = { onToggleEditMode() }) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = stringResource(id = R.string.exit_edit_mode_content_description)
+                        )
+                    }
+                } else {
+                    IconButton(onClick = onNavigateBack) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = stringResource(id = R.string.back_content_description)
+                        )
+                    }
+                }
+            }
+        }
+    )
+}
+
+@Composable
+private fun DetailsBottomBar(
+    isEditMode: Boolean,
+    onToggleEditMode: () -> Unit,
+    onSaveGiftDetails: () -> Unit
+) {
+    // TODO change the animation for another cool one
+    Crossfade(
+        targetState = isEditMode,
+        label = "bottom-bar-animation"
+    ) { isEditMode ->
+        val buttonColor = if (isEditMode) Color(0xFF4CAF50) else Color(0xFFC62828)
+        val buttonText =
+            if (isEditMode) stringResource(id = R.string.save_button) else stringResource(id = R.string.edit_button)
+        val onClickAction = if (isEditMode) {
+            { onSaveGiftDetails() }
+        } else {
+            { onToggleEditMode() }
+        }
+
+        Button(
+            onClick = onClickAction,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            shape = RoundedCornerShape(50),
+            colors = ButtonDefaults.buttonColors(containerColor = buttonColor)
+        ) {
+            Text(
+                text = buttonText,
+                modifier = Modifier.padding(8.dp),
+                fontSize = 16.sp
+            )
         }
     }
 }
